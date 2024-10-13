9d23281e4a496a7898c3081352da923b2fe86f4a
suman kumar <sumang91222@gmail.com>
2024-10-14 02:39:02 +0530

    credentials

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
+
 # config.py
 
 # Reddit API Credentials
-CLIENT_ID = "eR5L-MLDsYE2Gq1ID2QpBw"
-CLIENT_SECRET = "Pzbj6IcHzQ_7QcEnUpPXDWcvzkpotg"
-USER_AGENT = "trade_market_script"
-USERNAME = "Tall_Lavishness8215"
-PASSWORD = "Suman@123"
-
+CLIENT_ID = "your_client_id"
+CLIENT_SECRET = "your_client_secret"
+USER_AGENT = "your_user_agent"
+USERNAME = "your_reddit_username"
+PASSWORD = "your_reddit_password"
